afff0c6b212f562ad538df3d4a2cb43921463ca5
andre agel <andreagel9412@gmail.com>
2020-10-30 10:39:17 -0300

    Aplicando arquitetura mvvm na função salvar

--- app/src/main/java/br/com/alura/technews/repository/NoticiaRepository.kt
+++ app/src/main/java/br/com/alura/technews/repository/NoticiaRepository.kt
@@ -32,11 +32,16 @@ class NoticiaRepository(
 
 
     fun salva(
-        noticia: Noticia,
-        quandoSucesso: (noticiaNova: Noticia) -> Unit,
-        quandoFalha: (erro: String?) -> Unit
-    ) {
-        salvaNaApi(noticia, quandoSucesso, quandoFalha)
+        noticia: Noticia
+
+    ) :LiveData<Resource<Void?>>{
+        val liveData = MutableLiveData<Resource<Void?>>()
+        salvaNaApi(noticia, quandoSucesso={
+            liveData.value = Resource(null)
+        }, quandoFalha={
+            liveData.value = Resource(null, erro = it)
+        })
+        return liveData
     }
 
     fun remove(
